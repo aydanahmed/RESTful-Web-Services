79b1eb2bb40a32e721a0a6d95c3dc1471ce67d2d
Aydan <aydan.ahmed1994@gmail.com>
2018-07-17 14:59:08 +0300

    RESTful web services
    Simple RESTful web services

---
+++ messenger/src/main/java/org/koushik/javabrains/messenger/model/Profile.java
@@ -0,0 +1,72 @@
+package org.koushik.javabrains.messenger.model;
+
+import java.util.Date;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+public class Profile {
+	private long id;
+	private String profileName;
+	private String lastName;
+	private Date created;
+	
+	
+	public Profile() {
+		
+	}
+
+
+	public Profile(long id, String profileName, String lastName) {
+		super();
+		this.id = id;
+		this.profileName = profileName;
+		this.lastName = lastName;
+		this.created = new Date();
+	}
+
+
+	public long getId() {
+		return id;
+	}
+
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+
+	public String getProfileName() {
+		return profileName;
+	}
+
+
+	public void setProfileName(String profileName) {
+		this.profileName = profileName;
+	}
+
+
+	public String getLastName() {
+		return lastName;
+	}
+
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+
+	public Date getCreated() {
+		return created;
+	}
+
+
+	public void setCreated(Date created) {
+		this.created = created;
+	}
+	
+	
+	
+	
+	
+}
